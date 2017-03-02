2647ebcbcf97d7748d50b2d16b61f2f3af4d5051
Deep S Randhawa <deepsaranr@gmail.com>
2017-03-02 18:26:02 -0500

    Initial Commit

---
+++ RapBattle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RapBattle
+//
+//  Created by Deep S Randhawa on 3/2/17.
+//  Copyright © 2017 Deep S Randhawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
