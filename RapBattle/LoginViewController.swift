dfe8d7778814c5c80cc312d71bd69f8891e67fb6
Aditya Dhingra <adityadhingra07@gmail.com>
2017-04-25 12:41:09 -0400

    tweaks

--- RapBattle/LoginViewController.swift
+++ RapBattle/LoginViewController.swift
@@ -52,15 +52,21 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let borderColor = UIColor(red: 0, green: 205, blue: 0, alpha: 1)
+        let borderColor = UIColor(red: 0, green: 150, blue: 0, alpha: 1)
+        
+        email_field.attributedPlaceholder = NSAttributedString(string: "Email", attributes:[NSForegroundColorAttributeName: UIColor.gray])
+        password_field.attributedPlaceholder = NSAttributedString(string: "Password", attributes:[NSForegroundColorAttributeName: UIColor.gray])
+        
         
         email_field.layer.borderColor = borderColor.cgColor
         email_field.layer.cornerRadius = 5
         email_field.layer.masksToBounds = true
+        email_field.layer.borderWidth = 1.0
         
         password_field.layer.borderColor = borderColor.cgColor
         password_field.layer.cornerRadius = 5
         password_field.layer.masksToBounds = true
+        password_field.layer.borderWidth = 1.0
     }
 
     override func didReceiveMemoryWarning() {
