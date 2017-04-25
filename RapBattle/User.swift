66f62d1182cd03c0d7156e85c4f3eeb0b2f8e90d
monus <onus.muvaffak@gmail.com>
2017-04-25 00:56:08 -0400

    - little fix on user class

--- RapBattle/User.swift
+++ RapBattle/User.swift
@@ -20,7 +20,7 @@ class User {
     static let currentUser = User()
     
     func getUsername() -> String {
-        let periodIndex = self.email!.characters.index(of: ".")!
+        let periodIndex = self.email!.characters.index(of: "@")!
         return String(self.email!.characters.prefix(upTo: periodIndex))
     }
     
@@ -31,68 +31,4 @@ class User {
     func getAsDictionary() -> Dictionary<String, Any> {
         return ["email": self.email!]
     }
-    
-    /*private func getUserDataAsDict() -> [String: Any] {
-        return ["username"          : self.username,
-                "first_name"        : self.firstName,
-                "last_name"         : self.lastName,
-                //  "provider_id"       : self.providerID,
-            // "photo_url"         : self.photoURL!.absoluteString,
-            //"firebase_uid"      : self.firebaseUID,
-            "email"             : self.email]
-        //"experience_level"  : self.experienceLevel!.rawValue]
-    }*/
-    //private static var _currentUser: User?
-    /*static var currentUser: User? {
-        get {
-            if (_currentUser == nil) {
-                if FIRAuth.auth()?.currentUser != nil {
-                    let defaults = UserDefaults.standard
-                    if let userData = defaults.object(forKey: _userDefaultsKey) as? Data {
-                        let dict = try! JSONSerialization.jsonObject(with: userData, options: [])
-                        _currentUser = User.init(userData: dict as! Dictionary<String, Any>)
-                    }
-                }
-            }
-            return _currentUser
-        }
-        
-        set(user) {
-            _currentUser = user
-            
-            let defaults = UserDefaults.standard
-            if let user = user {
-                let data = try! JSONSerialization.data(withJSONObject: user.getUserDataAsDict(), options: [])
-                defaults.set(data, forKey: _userDefaultsKey)
-            } else {
-                defaults.removeObject(forKey: _userDefaultsKey)
-            }
-            defaults.synchronize()
-        }
-    }*/
-    
-  /*  private init(userData dict: Dictionary<String, Any>) {
-        self.username = dict["username"] as! String?
-        self.firstName = dict["first_name"] as! String?
-        self.lastName = dict["last_name"] as! String?
-        self.providerID = dict["provider_id"] as! String?
-        // self.photoURL = URL.init(string: (dict["photo_url"] as! String?)!)
-        self.firebaseUID = dict["firebase_uid"] as! String?
-        self.email = dict["email"] as! String?
-        self.experienceLevel = Experience.init(rawValue: (dict["experience_level"] as! String?)!)
-    }*/
-    
-   /* init(userInfo: FIRUserInfo, firstName: String, lastName: String, experienceLevel: Experience) {
-        self.username = userInfo.displayName
-        self.firstName = firstName
-        self.lastName = lastName
-        self.providerID = userInfo.providerID
-        // self.photoURL = userInfo.photoURL
-        self.firebaseUID = userInfo.uid
-        self.email = userInfo.email
-        self.experienceLevel = experienceLevel
-    }
-    */
-    
-    
 }
